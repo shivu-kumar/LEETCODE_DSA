6a097542d2ffedf69a6b700bd4392b696ebda205
Shivakumar <shivuaiet@gmail.com>
2025-10-08 23:19:32 +0530

    Time: 64 ms (89.04%) | Memory: 62.2 MB (46.12%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        int n=rains.length;
+        int ans[]=new int[n];
+        Arrays.fill(ans,1);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        TreeSet<Integer>set=new TreeSet<>();
+        for(int i=0;i<rains.length;i++){
+            if(rains[i]==0)
+            {
+                set.add(i);
+            }else{
+                int lake=rains[i];
+                ans[i]=-1;
+            if(map.containsKey(rains[i])){
+                Integer dryDay=set.higher(map.get(rains[i]));
+                if(dryDay==null)
+                return new int[0];
+
+                ans[dryDay]=lake;
+                set.remove(dryDay);
+               
+            }
+            map.put(lake,i);
+        }
+        }
+        return ans;
+    }
+}
